ece4cff747ff7dc58652a6a70d045f3744f16859
sanghpriya1 <sanghpriya2009@gmail.com>
2021-06-19 04:33:07 +0530

    Gui added

--- src/Client.java
+++ src/Client.java
@@ -1,13 +1,31 @@
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class Client {
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class Client extends JFrame {
 	Socket socket;
 	BufferedReader br;
 	PrintWriter out;
+
+	private JLabel heading = new JLabel("Client Area");
+	private JTextArea messaArea = new JTextArea();
+	private JTextField messageInput = new JTextField();
+	private Font font;
 
 	public Client() {
 		try {
@@ -16,12 +34,70 @@ public class Client {
 			System.out.println("Connection Done");
 			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			out = new PrintWriter(socket.getOutputStream());
+			createGUI();
+			handleEvents(); 
 			startReading();
-			startWriting();
+//			startWriting();
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	private void handleEvents() {
+		messageInput.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				//System.out.println("Key released" + e.getKeyCode());
+				if(e.getKeyCode() == 10) {
+					//System.out.println("you have pressed enter button");
+					String contetToSend = messageInput.getText(); 
+					messaArea.append("Me :" + contetToSend +"\n");
+					out.println(contetToSend);
+					out.flush();
+					messageInput.setText("");
+					messageInput.requestFocus();
+				}
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	}
+
+	private void createGUI() {
+		this.setTitle("Client Messanger[END]");
+		this.setSize(600, 600);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		heading.setFont(font);
+		messaArea.setFont(font);
+		messageInput.setFont(font);
+		heading.setIcon(new ImageIcon("chat.png"));
+		heading.setHorizontalTextPosition(SwingConstants.CENTER);
+		heading.setVerticalTextPosition(SwingConstants.BOTTOM);
+		heading.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		heading.setVerticalAlignment(SwingConstants.CENTER);
+		heading.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+		messageInput.setHorizontalAlignment(SwingConstants.CENTER );
+		this.setLayout(new BorderLayout());
+		this.add(heading, BorderLayout.NORTH);
+		this.add(messaArea, BorderLayout.CENTER);
+		this.add(messageInput, BorderLayout.SOUTH);
+  
+		this.setVisible(true);
 	}
 
 	private void startWriting() {
@@ -29,10 +105,10 @@ public class Client {
 			System.out.println("Writer started");
 
 			try {
-				while ( !socket.isClosed()) {
+				while (!socket.isClosed()) {
 					BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
 					String content = br1.readLine();
-					
+
 					out.println(content);
 					out.flush();
 					if (content.equals("exit")) {
@@ -54,23 +130,26 @@ public class Client {
 	private void startReading() {
 		Runnable r1 = () -> {
 			System.out.println("reader started");
-			
-				try {while (true) {
+
+			try {
+				while (true) {
 					String msg;
 					msg = br.readLine();
 					if (msg.equals("exit")) {
-						System.out.println("Client terminated the chat");
+						System.out.println("Server terminated the chat");
+						JOptionPane.showMessageDialog(this, "Server terminated the chat");
+						messageInput.setEnabled(false); 
 						socket.close();
 						break;
 					}
 					System.out.println("Server : " + msg);
-				} }catch (IOException e) {
-					// TODO Auto-generated catch block
-//					e.printStackTrace();
-					System.out.println("Connection Closed");
 				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+//					e.printStackTrace();
+				System.out.println("Connection Closed");
+			}
 
-			
 		};
 
 		new Thread(r1).start();
